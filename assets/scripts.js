ced3800a312ab25b4df043e7e3d06b870ea7e8d9
LHWOOD <hutsonwood@gmail.com>
2021-12-05 15:26:14 -0700

    started both weather and time calls

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,29 @@
-var yo = "yoyo";
+const timely = document.querySelector("#time");
 
-console.log(yo);
+function getTime() {
+  let now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+  let blah = document.createElement("h1");
+  blah.textContent = now;
+  timely.appendChild(blah);
+}
+
+getTime();
+
+function getWeather() {
+  let requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?zip=80127&units=imperial&appid=74f3ae2f827155ea451bd2e9b7cacb93";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      var tura = document.createElement("h5");
+      tura.textContent =
+        "The temperature is currently" + data.main.temp + " °F";
+      timely.appendChild(tura);
+    });
+}
+
+getWeather();
